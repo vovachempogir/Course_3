dce76ee552d9e40e45cfe35145e3c20dc0e31ceb
Vladimir <vovachempogir@mail.ru>
2023-04-16 19:22:13 +0700

    2

--- src/main/java/pro/sky/socksapp/services/impl/SocksFilesServiceImpl.java
+++ src/main/java/pro/sky/socksapp/services/impl/SocksFilesServiceImpl.java
@@ -70,4 +70,5 @@ public class SocksFilesServiceImpl implements SocksFilesService {
             return false;
         }
     }
+
 }
--- src/main/java/pro/sky/socksapp/services/impl/SocksServiceImpl.java
+++ src/main/java/pro/sky/socksapp/services/impl/SocksServiceImpl.java
@@ -106,4 +106,5 @@ public class SocksServiceImpl implements SocksService {
             throw new RuntimeException(e);
         }
     }
+
 }
